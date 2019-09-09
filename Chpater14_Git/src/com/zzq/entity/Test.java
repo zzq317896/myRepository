67008ab624c4d622cb947f9fe407d0ff909085df
zzq <lenovo@LAPTOP-AHSOCJ2F>
2019-09-09 20:04:45 +0800

    2019-09-09 张自权创建

---
+++ Chpater14_Git/src/com/zzq/entity/Test.java
@@ -0,0 +1,11 @@
+package com.zzq.entity;
+/**
+ *@author 张自权 
+ *@version 创建时间：2019年9月9日 下午8:02:39 
+*/
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("李佳怡是我的大傻子..");
+	}
+}
